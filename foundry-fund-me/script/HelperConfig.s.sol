57da82f09862950fc1161800f040288fe4369c8b
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2026-02-19 17:45:54 +0530

    feat: add mock aggregator for anvil testnet

--- foundry-fund-me/script/HelperConfig.s.sol
+++ foundry-fund-me/script/HelperConfig.s.sol
@@ -3,8 +3,11 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
+    uint8 constant DECIMAL = 8;
+    int256 constant INITIAL_PRICE = 2000e8;
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
@@ -31,9 +34,28 @@ contract HelperConfig is Script {
     function getMainnetEthConfig() public pure {
         // Price feed address
         NetworkConfig memory mainnetEthConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            priceFeed: 0x54EdE484Bb0E589F5eE13e04c84f46eb787c9C6a
         });
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+
+        return anvilConfig;
+    }
 }
